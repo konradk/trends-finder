da994349e602a0394e37e257d7369140b49cd000
Konrad Kruk <github@konradk.pl>
2022-09-02 16:27:11 +0200

    make charts responsive

--- src/Chart.js
+++ src/Chart.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import '../node_modules/react-vis/dist/style.css';
+import "../node_modules/react-vis/dist/style.css";
 import {
   XYPlot,
   XAxis,
@@ -7,18 +7,28 @@ import {
   VerticalGridLines,
   HorizontalGridLines,
   VerticalBarSeries,
+  makeWidthFlexible,
 } from "react-vis";
 
 const Chart = ({ data }) => {
+  const Plot = makeWidthFlexible(XYPlot);
   return (
-    <XYPlot xType="ordinal" height={300} width={800}>
+    <Plot xType="ordinal" height={300}>
       <VerticalGridLines />
       <HorizontalGridLines />
-      <XAxis tickFormat={(value) => value.split('-')[1] === '01' ? value.split('-')[0] : (data.length < 13 ? value.split('-')[1] : '')} />
+      <XAxis
+        tickFormat={(value) =>
+          value.split("-")[1] === "01"
+            ? value.split("-")[0]
+            : data.length < 13
+            ? value.split("-")[1]
+            : ""
+        }
+      />
       <YAxis />
       <VerticalBarSeries data={data} />
       {/* <LabelSeries data={data} getLabel={d => d.x} /> */}
-    </XYPlot>
+    </Plot>
   );
 };
 
